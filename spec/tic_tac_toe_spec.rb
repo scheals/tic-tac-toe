a02de7e691f6652e3b563927ae9cd88324f0e529
Scheals <piecoo@gmail.com>
2022-04-08 20:56:52 +0200

    Introduce win? tests

--- spec/tic_tac_toe_spec.rb
+++ spec/tic_tac_toe_spec.rb
@@ -1,22 +1,55 @@
 # frozen_string_literal: true
 
-require_relative '../lib/tic_tac_toe'
+# rubocop:disable Metrics/BlockLength
+
+require_relative '../lib/tictactoe'
 require_relative '../lib/player'
+require_relative '../lib/gameboard'
 
-describe TicTacToe do
-  describe '#add_players' do
-    context 'when players are already added' do
-      subject(:playerful_game) { described_class.new }
-      let(:player) { Player.new }
-
+describe Gameboard do
+  describe '#win?' do
+    context 'when no win condition has been reached' do
+      subject(:mid_game) { described_class.new }
       before do
-        playerful_game.add_players(player, player)
+        mid_game.make_move('bottom left', 'O')
+        mid_game.make_move('middle middle', 'X')
+        mid_game.make_move('bottom right', 'O')
       end
-
-      it 'executes an early return with an error message' do
-        playerful_game.add_players(player, player)
-        error_message = "#{player1_name} and #{player2_name} are already playing!"
-        expect(playerful_game).to be(error_message)
+      it 'returns false' do
+        expect(mid_game.win?).to be(false)
+      end
+    end
+    context 'when there are three of the same sign in a row' do
+      subject(:game_row) { described_class.new }
+      before do
+        game_row.make_move('bottom left', 'O')
+        game_row.make_move('bottom right', 'O')
+        game_row.make_move('bottom middle', 'O')
+      end
+      it 'returns true' do
+        expect(game_row.win?).to be(true)
+      end
+    end
+    context 'when there are three of the same sign in a column' do
+      subject(:game_column) { described_class.new }
+      before do
+        game_column.make_move('bottom left', 'O')
+        game_column.make_move('middle left', 'O')
+        game_column.make_move('top left', 'O')
+      end
+      it 'returns true' do
+        expect(game_column.win?).to be(true)
+      end
+    end
+    context 'when there are three of the same sign in a diagonal' do
+      subject(:game_diagonal) { described_class.new }
+      before do
+        game_diagonal.make_move('bottom left', 'O')
+        game_diagonal.make_move('middle middle', 'O')
+        game_diagonal.make_move('top right', 'O')
+      end
+      it 'returns true' do
+        expect(game_diagonal.win?).to be(true)
       end
     end
   end
